a5ced78ea5dd9d574709beb813f84cb524cd348c
Nishant-Dave <nishant.nsbda@gmail.com>
2025-09-17 23:19:53 +0530

    portfolio model and admin panel set

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    short_description = models.CharField(max_length=300)
+    body = models.TextField()
+    github_url = models.URLField(blank=True, null=True)
+    demo_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='project_images/')
+    caption = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"Image for {self.project.title}"
+    
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    proficiency = models.IntegerField(help_text="Proficiency level from 1 to 100")
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return f"{self.name} (Proficiency: {self.proficiency}%)"
