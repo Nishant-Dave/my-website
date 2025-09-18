d6a763b01536e5fe6f1c915e03e0df90f224e260
Nishant-Dave <nishant.nsbda@gmail.com>
2025-09-18 18:57:09 +0530

    display project details and image on the template

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -4,3 +4,4 @@ from .models import Project, ProjectImage, Skill
 admin.site.register(Project)
 admin.site.register(ProjectImage)
 admin.site.register(Skill)
+
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Project, Skill
 
-# Create your views here.
+def portfolio_home(request):
+    projects = Project.objects.all()
+    skills = Skill.objects.all()
+    return render(request, 'portfolio/home.html', {'projects': projects, 'skills': skills})
+
+def project_detail(request, slug):
+    project = Project.objects.get(slug=slug)
+    return render(request, 'portfolio/project_detail.html', {'project': project})
+
